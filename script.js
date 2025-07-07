b37f8b5f7ada6943ead6fae9e90301cdc3398f03
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-07-07 16:45:07 -0600

    Create functions to add books to array of books
    Create functions and modify the array declaration to use let instead
    of const.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const myLibrary = [];
+let myLibrary = [];
+let i = 0;
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary[i++] = new Book(title, author, pages, read);
+}

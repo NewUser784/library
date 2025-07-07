c31a5db807f27dba0404c19e1e99813b8ec7b75c
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-07-07 17:37:21 -0600

    Show all of the book's data on its card

--- script.js
+++ script.js
@@ -21,11 +21,30 @@ function addBookToLibrary(title, author, pages, read) {
 function displayBooks() {
     myLibrary.forEach((book) => {
         let cardDiv = document.createElement("div");
-        cardDiv.textContent = book.title;
+        cardDiv.classList.toggle("card");
+        let cardIcon = document.createElement("div");
+        cardIcon.classList.toggle("card-icon");
+        let cardList = document.createElement("ul");
+        let title = document.createElement("li");
+        title.textContent = book.title;
+        let author = document.createElement("li");
+        author.textContent = book.author;
+        let pages = document.createElement("li");
+        pages.textContent = book.pages;
+        let read = document.createElement("li");
+        read.textContent = book.read;
         cardContainer.appendChild(cardDiv);
+        cardDiv.appendChild(cardIcon);
+        cardDiv.appendChild(cardList);
+        cardList.appendChild(title);
+        cardList.appendChild(author);
+        cardList.appendChild(pages);
+        cardList.appendChild(read);
     });
 }
 
 // addBookToLibrary("Mistborn", "Sanderson", 500, "Not read yet");
 // addBookToLibrary("Blood of Elves", "Sapkwoski", 600, "Read");
+// addBookToLibrary("Fellowship of the Ring", "Tolkien", 1000, "Not read yet");
+// addBookToLibrary("Goblet of Fire", "Rowling", 500, "Read");
 // displayBooks();

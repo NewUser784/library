05848c6eb64950923bc9bd8021e48dd9e5b1ac98
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-07-07 17:03:25 -0600

    Create basic function to display the books
    Create function that displays the books' titles.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 let i = 0;
+let cardContainer = document.querySelector(".cards");
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -16,3 +17,15 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     myLibrary[i++] = new Book(title, author, pages, read);
 }
+
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        let cardDiv = document.createElement("div");
+        cardDiv.textContent = book.title;
+        cardContainer.appendChild(cardDiv);
+    });
+}
+
+// addBookToLibrary("Mistborn", "Sanderson", 500, "Not read yet");
+// addBookToLibrary("Blood of Elves", "Sapkwoski", 600, "Read");
+// displayBooks();

c30954ea200d984816b44f079db2e4c560133856
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-07-29 11:49:29 -0600

    Allow the user to add a new book to the array
    Make it so a book with the data provided by the user through the dialog
    is added to the library array and that it's also displayed on the page.
    Add an attribute to the constructor of the Book object that keeps
    track of whether the book is already being displayed. this
    attribute is initially set to 0 (not displayed) and it gets changed to
    a 1 after it gets displayed; this is done so that it doesn't get
    displayed multiple times. Add an if to check this value in the
    method that displays the books.

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let cardContainer = document.querySelector(".cards");
 let newBookBut = document.querySelector(".new-book");
 let dialog = document.querySelector("dialog");
 let cancelBut = document.querySelector(".cancel-but");
+let addBookBut = document.querySelector(".add-book-but");
+let titleInput = document.querySelector("#book_title");
+let authorInput = document.querySelector("#book_author");
+let pagesInput = document.querySelector("#book_pages");
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, displayStatus) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -15,49 +19,54 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.displayStatus = displayStatus;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary[i++] = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, displayStatus) {
+    myLibrary[i++] = new Book(title, author, pages, read, displayStatus);
 }
 
 function displayBooks() {
     myLibrary.forEach((book) => {
-        let cardDiv = document.createElement("div");
-        cardDiv.classList.toggle("card");
-        let cardIcon = document.createElement("div");
-        cardIcon.classList.toggle("card-icon");
-        let cardList = document.createElement("ul");
+        if (book.displayStatus !== 1) {
+            let cardDiv = document.createElement("div");
+            cardDiv.classList.toggle("card");
+            let cardIcon = document.createElement("div");
+            cardIcon.classList.toggle("card-icon");
+            let cardList = document.createElement("ul");
 
-        let titleText = document.createElement("p");
-        titleText.textContent = book.title;
-        let authorText = document.createElement("p");
-        authorText.textContent = book.author;
-        let pagesText = document.createElement("p");
-        pagesText.textContent = book.pages;
-        let readText = document.createElement("p");
-        readText.textContent = book.read;
+            let titleText = document.createElement("p");
+            titleText.textContent = book.title;
+            let authorText = document.createElement("p");
+            authorText.textContent = book.author;
+            let pagesText = document.createElement("p");
+            pagesText.textContent = book.pages;
+            let readText = document.createElement("p");
+            readText.textContent = book.read;
 
-        let titleHeader = document.createElement("li");
-        titleHeader.textContent = "Title: ";
-        let authorHeader = document.createElement("li");
-        authorHeader.textContent = "Author: ";
-        let pagesHeader = document.createElement("li");
-        pagesHeader.textContent = "Pages: ";
-        let readHeader = document.createElement("li");
-        readHeader.textContent = "Status: ";
+            let titleHeader = document.createElement("li");
+            titleHeader.textContent = "Title: ";
+            let authorHeader = document.createElement("li");
+            authorHeader.textContent = "Author: ";
+            let pagesHeader = document.createElement("li");
+            pagesHeader.textContent = "Pages: ";
+            let readHeader = document.createElement("li");
+            readHeader.textContent = "Status: ";
 
-        cardContainer.appendChild(cardDiv);
-        cardDiv.appendChild(cardIcon);
-        cardDiv.appendChild(cardList);
-        cardList.appendChild(titleHeader);
-        titleHeader.appendChild(titleText);
-        cardList.appendChild(authorHeader);
-        authorHeader.appendChild(authorText);
-        cardList.appendChild(pagesHeader);
-        pagesHeader.appendChild(pagesText);
-        cardList.appendChild(readHeader);
-        readHeader.appendChild(readText);
+            cardContainer.appendChild(cardDiv);
+            cardDiv.appendChild(cardIcon);
+            cardDiv.appendChild(cardList);
+            cardList.appendChild(titleHeader);
+            titleHeader.appendChild(titleText);
+            cardList.appendChild(authorHeader);
+            authorHeader.appendChild(authorText);
+            cardList.appendChild(pagesHeader);
+            pagesHeader.appendChild(pagesText);
+            cardList.appendChild(readHeader);
+            readHeader.appendChild(readText);
+
+            book.displayStatus = 1;
+        }
     });
 }
 
@@ -69,8 +78,14 @@ cancelBut.addEventListener("click", () => {
     dialog.close();
 });
 
-// addBookToLibrary("Mistborn", "Sanderson", 500, "Not read yet");
-// addBookToLibrary("Blood of Elves", "Sapkwoski", 600, "Read");
-// addBookToLibrary("Fellowship of the Ring", "Tolkien", 1000, "Not read yet");
-// addBookToLibrary("Goblet of Fire", "Rowling", 500, "Read");
-// displayBooks();
+addBookBut.addEventListener("click", () => {
+    let statusRadio = document.querySelector("input[name='status']:checked");
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, statusRadio.value, 0);
+    displayBooks();
+});
+
+addBookToLibrary("Mistborn", "Sanderson", 500, "Not read yet", 0);
+addBookToLibrary("Blood of Elves", "Sapkwoski", 600, "Read", 0);
+addBookToLibrary("Fellowship of the Ring", "Tolkien", 1000, "Not read yet", 0);
+addBookToLibrary("Goblet of Fire", "Rowling", 500, "Read", 0);
+displayBooks();
